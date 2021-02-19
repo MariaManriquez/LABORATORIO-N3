8023ad7aa4544c96bc266fd217cd7973ccd692f8
Maria Manriquez <manriquez22446688@gmail.com>
2021-02-19 10:36:51 -0400

    app.js modificado

--- app.js
+++ app.js
@@ -22,6 +22,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+app.use('servicee',service);
+
+var service = require('./routes/service');
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -36,6 +40,10 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+const port = 8000;
+app.listen(port, ()=>{
+  console.log("Corriendo en el puerto" + port)
 });
 
 module.exports = app;
